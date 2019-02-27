05bf0ee3070867373c6bcbb8ae223ca0121bbe5e
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-27 12:32:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+class Driver {
 
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
+
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - `${this.startDate}`.match(/\d{4}/);
+  };
+};
+
+class Route {
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    let endAve = eastWest.findIndex(ave => ave === this.endingLocation.horizontal);
+
+    let startAve = eastWest.findIndex(ave => ave === this.beginningLocation.horizontal);
+
+    return (endAve - startAve) + (this.endingLocation.vertical - this.beginningLocation.vertical);
+  };
+
+  estimatedTime(peakTime) {
+    return peakTime ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3;
+  };
+};
